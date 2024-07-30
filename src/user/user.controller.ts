2d37d04dc87363effab73e7044a7dab5e3f12817
Denys Markevych <denysjse@gmail.com>
2024-07-30 10:23:00 +0300

    Create user controller (not finished)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserDto } from './dto/user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile/:id')
+	// @Auth()  //@CurrentUser('id') id: number
+	getProfile(@Param('id') id: number) {
+		return this.userService.getUserById(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put('profile/:id')
+	// @Auth()  //@CurrentUser('id') id: number
+	updateProfile(@Param('id') id: number, @Body() dto: UpdateUserDto) {
+		return this.userService.updateProfile(id, dto)
+	}
+
+	@Delete('profile/:id')
+	// @Auth()  //@CurrentUser('id') id: number)
+	deleteProfile(@Param('id') id: number) {
+		return this.userService.deleteProfile(id)
+	}
+
+	// todo: create saveDishToFavorite endpoint when dist service will be done!!!
 }
