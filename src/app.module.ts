0e74bdd89a25cf3fdb40fefa76afd75a8584c300
Denys Markevych <denysjse@gmail.com>
2024-07-30 09:09:37 +0300

    Make base setting for main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	controllers: [AppController],
+	providers: [AppService, PrismaService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const PORT = process.env.PORT || 7777
+
+	const app = await NestFactory.create(AppModule)
+
+	app.setGlobalPrefix('api')
+	app.enableCors()
+	app.use(cookieParser())
+
+	await app.listen(PORT, () =>
+		console.log(`Server started in the port: ${PORT}`)
+	)
 }
-bootstrap();
+bootstrap()
