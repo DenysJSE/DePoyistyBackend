12905893009baf0bd0e4aee081b88ee968d374da
Denys Markevych <denysjse@gmail.com>
2024-08-06 13:53:18 +0300

    Create docs for review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -12,23 +12,48 @@ import { ReviewService } from './review.service'
 import { Auth } from '../auth/decorators/auth.decorator'
 import { CurrentUser } from '../auth/decorators/user.decorator'
 import { ReviewDto } from './dto/review.dto'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import {
+	RatingValueResponse,
+	ReviewCreationResponse,
+	ReviewResponse
+} from '../docs/review-response'
 
+@ApiTags('Reviews')
 @Controller('reviews')
 export class ReviewController {
 	constructor(private readonly reviewService: ReviewService) {}
 
+	@ApiOperation({ summary: 'Get all reviews about dish' })
+	@ApiResponse({
+		status: 200,
+		type: [ReviewResponse],
+		description: 'The list of review about dish'
+	})
 	@Auth('admin')
 	@Get('dish')
 	getAllDishReviews() {
 		return this.reviewService.getAllDishReviews()
 	}
 
+	@ApiOperation({ summary: 'Get all reviews about restaurant' })
+	@ApiResponse({
+		status: 200,
+		type: [ReviewResponse],
+		description: 'The list of review about restaurant'
+	})
 	@Auth('admin')
 	@Get('restaurant')
 	getAllRestaurantReview() {
 		return this.reviewService.getAllRestaurantReviews()
 	}
 
+	@ApiOperation({ summary: 'Leave review about dish' })
+	@ApiResponse({
+		status: 200,
+		type: ReviewCreationResponse,
+		description: 'Review response after user crete some review'
+	})
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Auth()
@@ -41,6 +66,12 @@ export class ReviewController {
 		return this.reviewService.leaveDishReview(userId, +dishId, reviewDto)
 	}
 
+	@ApiOperation({ summary: 'Leave review about restaurant' })
+	@ApiResponse({
+		status: 200,
+		type: ReviewCreationResponse,
+		description: 'Review response after user crete some review'
+	})
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Auth()
@@ -57,11 +88,23 @@ export class ReviewController {
 		)
 	}
 
+	@ApiOperation({ summary: 'Get rate of dish' })
+	@ApiResponse({
+		status: 200,
+		type: RatingValueResponse,
+		description: 'Rate of dish'
+	})
 	@Get('average-for-dish/:dishId')
 	getAverageRatingForDish(@Param('dishId') dishId: string) {
 		return this.reviewService.getAverageRatingValueForDish(+dishId)
 	}
 
+	@ApiOperation({ summary: 'Get rate of restaurant' })
+	@ApiResponse({
+		status: 200,
+		type: RatingValueResponse,
+		description: 'Rate of restaurant'
+	})
 	@Get('average-for-restaurant/:restaurantId')
 	getAverageRatingForRestaurant(@Param('restaurantId') restaurantId: string) {
 		return this.reviewService.getAverageRatingValueForRestaurant(+restaurantId)
