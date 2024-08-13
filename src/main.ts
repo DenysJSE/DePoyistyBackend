0586d111e6760f47fe37b8e4d01866b944ba767a
Denys Markevych <denysjse@gmail.com>
2024-08-13 16:02:48 +0300

    Add hosted frontend link to cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
 
 	app.setGlobalPrefix('api')
 	app.enableCors({
-		origin: 'http://localhost:5173',
+		origin: ['http://localhost:5173', 'https://depoyisty.netlify.app'],
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		credentials: true
 	})
 	app.use(cookieParser())
